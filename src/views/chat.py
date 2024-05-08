76e3eddeed2ef9097c5d6a2d7e2f56bd082b4985
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-05-08 17:03:54 +0200

    feature: add htmx and mustache templates

--- src/views/chat.py
+++ src/views/chat.py
@@ -9,13 +9,18 @@ router = APIRouter(
     tags=["Chat"],
 )
 
+@router.get("/conversation/{session_id}")
+def get_conversation(session_id: str):
+    message_history_service = MessageHistoryService(session_id)
+    conversation = message_history_service.get_messages_by_session_id()
+    return [{"content": message.content, "type": message.type} for message in conversation]
 
 @router.get("/", response_class=HTMLResponse)
 def chat(request: Request):
     message_history_service = MessageHistoryService()
 
     conversations = message_history_service.get_unique_session_ids()
-    print(conversations)
+
     return settings.TEMPLATES.TemplateResponse(
         request=request,
         name="chat.html",
