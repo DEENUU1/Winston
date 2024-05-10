118c1d77896aa0738f37798e1f84921199c93be3
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-05-10 19:12:45 +0200

    feature: create schemas for Settings model

---
+++ src/backend/schemas/settings.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class SettingsInputSchema(BaseModel):
+    agent_id: int
+
+
+class SettingsOutputSchema(BaseModel):
+    id: int
+    agent_id: int
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
